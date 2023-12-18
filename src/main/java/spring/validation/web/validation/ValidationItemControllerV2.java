4a512b870689534cb303d18e8c0c65586673b6ef
hyun <hyun.choi499@gmail.com>
2023-12-18 14:31:24 +0900

    Validator 도입

--- src/main/java/spring/validation/web/validation/ValidationItemControllerV2.java
+++ src/main/java/spring/validation/web/validation/ValidationItemControllerV2.java
@@ -27,6 +27,7 @@ import spring.validation.domain.item.ItemRepository;
 public class ValidationItemControllerV2 {
 
 	private final ItemRepository itemRepository;
+	private final ItemValidator itemValidator;
 
 	@GetMapping
 	public String items(Model model) {
@@ -140,7 +141,7 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+//	@PostMapping("/add")
 	public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 		if (!StringUtils.hasText(item.getItemName())) {
 			bindingResult.rejectValue("itemName", "required");
@@ -159,6 +160,22 @@ public class ValidationItemControllerV2 {
 				bindingResult.reject("totalPriceMin", new Object[] { 10000 }, null);
 			}
 		}
+
+		if (bindingResult.hasErrors()) {
+			log.debug("bindingResult: {}", bindingResult);
+			return "validation/v2/addForm";
+		}
+
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@PostMapping("/add")
+	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+		itemValidator.validate(item, bindingResult);
 
 		if (bindingResult.hasErrors()) {
 			log.debug("bindingResult: {}", bindingResult);
